ee73e8104c5569c1f2937bbdae060b263614886e
cyrusbuffington <cyrusbuffington@gmail.com>
2024-11-03 18:39:54 -0800

    Navigation To Counter

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
+
+  const navigateToCounter = () => {
+    router.navigate("/counter");
+  }
+
   return (
     <View
       style={{
@@ -11,6 +17,24 @@ export default function Index() {
       }}
     >
       <Text>Welcome to ZotBins!</Text>
+      <TouchableOpacity 
+        onPress={navigateToCounter}
+        style={{
+          backgroundColor: "orange",
+          borderRadius: 5,
+          margin: 10,
+          paddingHorizontal: 55,
+          paddingVertical: 20
+        }}
+      >
+        <Text
+          style={{
+            color: "white"
+          }}>
+          View Counter
+        </Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
