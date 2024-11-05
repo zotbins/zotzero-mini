c6eeb28808301af0dce1631d517cdd7e57d8696a
EthanSantos <ethanangelo61@gmail.com>
2024-11-04 20:54:12 -0800

    finished counter

--- app/(tabs)/counter.tsx
+++ app/(tabs)/counter.tsx
@@ -1,16 +1,39 @@
-import React from "react";
-import { View, Text, Pressable } from "react-native";
+import { useState } from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Let's make a counter here.</Text>
+    <View style={styles.viewStyle}>
+      <Text>Counter: {count}</Text>
+      <View>
+        <Pressable style = {styles.button} onPress={() => setCount(count + 1)}>
+          <Text style = {styles.buttonText}> Increase </Text>
+        </Pressable>
+        <Pressable style = {styles.button} onPress={() => setCount(count - 1)}>
+          <Text style = {styles.buttonText}> Decrease </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "blue",
+    margin : 2.5,
+    paddingHorizontal: 65,
+    paddingVertical: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: 'center'
+  }
+})
